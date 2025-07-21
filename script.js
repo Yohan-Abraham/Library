aabd8124f0bfd0d98d8ee5784eba3e377ec118ea
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2025-07-20 17:10:02 -0700

    Basic Layout
    Created basic layout

---
+++ script.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    for (const book of myLibrary) {
+        console.log(book);
+    }
+}
+
+const addBook = document.querySelector("#add-book");
+
