d20a4065b09cacc7af9e95a8a46a87515f95db1f
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2025-07-20 18:56:21 -0700

    Complete Display Book Function
    Finished display book function

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 const myLibrary = [];
 
-function Book(name, author, pages, read) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+function addBookToLibrary(title, author, pages, read) {
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
 function displayBooks() {
+    const bookContainer = document.querySelector("#books");
+
     for (const book of myLibrary) {
-        console.log(book);
+        const card = document.createElement("div");
+        card.id = crypto.randomUUID();
+        card.className = "card";
+
+        const title = document.createElement("div");
+        title.className = "title";
+        title.textContent = `Title: ${book.title}`;
+        card.appendChild(title);
+
+        const author = document.createElement("div");
+        author.className = "author";
+        author.textContent = `Author: ${book.author}`;
+        card.appendChild(author);
+
+        const pages = document.createElement("div");
+        pages.className = "pages";
+        pages.textContent = `Pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        const read = document.createElement("div");
+        read.className = "read";
+        read.textContent = `Read: ${book.read}`;
+        card.appendChild(read);
+
+        bookContainer.appendChild(card);
+
     }
 }
 
-const addBook = document.querySelector("#add-book");
 
+addBookToLibrary("The prince", "Machivelli", 200, false);
+displayBooks();
+
+addBook.addEventListener("click", () => {
+
+}); 
